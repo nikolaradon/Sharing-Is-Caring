dd557fb4a939847398e694b6b4ec0d2ab3cfcac2
nikolaradon <nikola.radon@gmail.com>
2024-01-11 17:09:56 +0100

    added views for LandingPage AddDonation Login Register

--- SharingIsCaring/SharingIsCaringApp/views.py
+++ SharingIsCaring/SharingIsCaringApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'index2.html')
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form2.html')
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')

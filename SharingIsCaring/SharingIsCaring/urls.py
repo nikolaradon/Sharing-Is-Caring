9838c57a943e6d471ed12dc9454627ab29ce1748
nikolaradon <nikola.radon@gmail.com>
2024-01-17 13:20:13 +0100

    additonal files

--- SharingIsCaring/SharingIsCaring/urls.py
+++ SharingIsCaring/SharingIsCaring/urls.py
@@ -16,12 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from SharingIsCaringApp.views import (LandingPageView, AddDonationView, LoginView, RegisterView)
+from SharingIsCaringApp.views import (LandingPageView, AddDonationView, LoginView, RegisterView,
+                                      StepsView, AboutUsView, HelpView, FormConfirmationView
+                                      )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='index'),
-    path('add_donation/', AddDonationView.as_view(), name='add_donation'),
+    path('add_donation/', AddDonationView.as_view(), name='form'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('steps/', StepsView.as_view(), name='steps'),
+    path('about_us/', AboutUsView.as_view(), name='about_us'),
+    path('help/', HelpView.as_view(), name='help'),
+    path('confirmation', FormConfirmationView.as_view(), name='help'),
 ]
--- SharingIsCaring/SharingIsCaringApp/views.py
+++ SharingIsCaring/SharingIsCaringApp/views.py
@@ -4,18 +4,39 @@ from django.views import View
 
 # Create your views here.
 
+
 class LandingPageView(View):
     def get(self, request):
         return render(request, 'index2.html')
+
 
 class AddDonationView(View):
     def get(self, request):
         return render(request, 'form2.html')
 
+
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html')
 
+
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+
+
+class StepsView(View):
+    pass
+
+
+class AboutUsView(View):
+    pass
+
+
+class HelpView(View):
+    pass
+
+
+class FormConfirmationView(View):
+    def get(self, request):
+        return render(request, 'form-confirmation2.html')

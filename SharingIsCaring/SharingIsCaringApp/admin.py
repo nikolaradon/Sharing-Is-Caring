92e8b250d1ffd5bfb1eedd8472dc5ff38553174b
nikolaradon <nikola.radon@gmail.com>
2024-01-23 16:43:07 +0100

    model Imstitution registered in admin panel

--- SharingIsCaring/SharingIsCaringApp/admin.py
+++ SharingIsCaring/SharingIsCaringApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Institution
 
 # Register your models here.
+
+
+@admin.register(Institution)
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'type', 'display_categories')
+
+
+    def display_categories(self, obj):
+            return ", ".join([category.name for category in obj.categories.all()])
--- SharingIsCaring/SharingIsCaringApp/models.py
+++ SharingIsCaring/SharingIsCaringApp/models.py
@@ -19,10 +19,7 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.CharField(max_length=30, choices=INSTITUTION_TYPES, default='fundacja')
     categories = models.ManyToManyField(Category)
-
-
-from django.db import models
-from django.contrib.auth.models import User
+    
 
 
 class Donation(models.Model):
